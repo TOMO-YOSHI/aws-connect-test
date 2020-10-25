f3e41aeac2d0a5e6f4c7e28e131bd9959b947b0b
TOMO-YOSHI <lucky.paddy10@gmail.com>
2020-10-25 06:38:30 -0700

    push test

---
+++ react-project/src/components/Component.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+
+const Component = () => {
+    const [data, setData] = useState([])
+
+    useEffect(()=>{
+        // fetch("http://localhost:8080/api/pets", { mode: "cors" })
+        fetch("http://52.27.196.252:8080/api/pets", { mode: "cors" })
+          .then((result) => {
+            // console.log(result.json());
+            // setData(result);
+            return result.json();
+          })
+          .then((result) => {
+            console.log(result);
+            setData(result);
+          })
+          .catch((error) => console.log(error));
+    }, [])
+
+    return (
+        <div>
+            {data.map((el, i) => {
+                return <p key={i}>{el.pet_name}</p>
+            })}
+        </div>
+    );
+}
+
+export default Component;
